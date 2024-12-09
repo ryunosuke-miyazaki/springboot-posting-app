a7b229891891fbe12065893317a9ab053214d72c
ryunosuke-miyazaki <rymiyazaki@japanet.co.jp>
2024-12-09 16:47:00 +0900

    Add files via upload

---
+++ postingapp/src/main/java/com/example/postingapp/repository/PostRepository.java
@@ -0,0 +1,17 @@
+package com.example.postingapp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.postingapp.entity.Post;
+import com.example.postingapp.entity.User;
+
+public interface PostRepository extends JpaRepository<Post, Integer>{
+	public List<Post> findByUserOrderByCreatedAtDesc(User user);
+	public Post findFirstByOrderByIdDesc();
+	
+	//課題追記分
+	public List<Post> findByUserOrderByUpdatedAt(User user);
+
+}
